d28f75a8a0f9c4e8d47cc3053d71bf113f210619
TheBOSS <hossamhaida4@gmail.com>
2025-03-29 22:14:02 +0000

    added search

--- src/main/java/org/enset/hopital/Repositoty/PatientRepository.java
+++ src/main/java/org/enset/hopital/Repositoty/PatientRepository.java
@@ -1,8 +1,10 @@
 package org.enset.hopital.Repositoty;
 
 import org.enset.hopital.Entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
 }
