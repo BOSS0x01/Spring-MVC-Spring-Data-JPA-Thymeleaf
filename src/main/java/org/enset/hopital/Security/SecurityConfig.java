51f4ee30f4f9f2d509270de61225c238f993cbca
TheBOSS <hossamhaida4@gmail.com>
2025-04-13 01:04:23 +0100

    added spring security implemented inMemoryAuthentication added custom login page added filter chain

---
+++ src/main/java/org/enset/hopital/Security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package org.enset.hopital.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user").password(passwordEncoder.encode( "1234")).roles("USER").build(),
+                User.withUsername("user1").password(passwordEncoder.encode( "1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode( "1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode( "1234")).roles("ADMIN","USER").build()
+
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/login").permitAll();
+        http.rememberMe().rememberMeParameter("remember-me");
+        http.authorizeHttpRequests()
+                .requestMatchers("/webjars/**", "/css/**", "/js/**", "/images/**").permitAll()
+                .requestMatchers("/login", "/logout").permitAll()
+                .requestMatchers("/user/**").hasRole("USER")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        http.exceptionHandling().accessDeniedPage("/notAuthorized");
+
+        return http.build();
+    }
+
+}
--- src/main/java/org/enset/hopital/Web/PatientController.java
+++ src/main/java/org/enset/hopital/Web/PatientController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PatientController {
 private PatientRepository patientRepository;
 
-    @GetMapping("/index")
+    @GetMapping({"/", "/user/index"})
     public String index(Model model, @RequestParam(name = "page",defaultValue = "0") int page,
                         @RequestParam(name="size",defaultValue="5") int size,
                         @RequestParam(name = "keyword",defaultValue= "") String keyword) {
@@ -30,31 +30,35 @@ private PatientRepository patientRepository;
         return "Patients";
     }
 
-    @GetMapping("/delete")
-    public String delete(Long id,String keyword,int page) {
+    @GetMapping("/admin/delete")
+    public String delete(Long id, String keyword,int page) {
         patientRepository.deleteById(id);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
     }
-    @GetMapping("/addPatient")
+
+    @GetMapping("/admin/addPatient")
     public String addPatient(Model model) {
         model.addAttribute("patient", new Patient());
         model.addAttribute("action","Add");
         return "formPatient";
     }
 
-    @PostMapping("/save")
-    public String save(Model model, @Valid Patient patient , BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) return "redirect:/addPatient";
-        patientRepository.save(patient);
-        return "redirect:/index";
-    }
-
-    @GetMapping("/editPatient")
-    public String editPatient(Model model, Long id) {
+    @GetMapping("/admin/editPatient")
+    public String editPatient(Model model, Long id,String keyword,int page) {
         Patient patient = patientRepository.findById(id).orElse(null);
         if (patient == null) throw new RuntimeException("Patient not found");
         model.addAttribute("patient", patient);
         model.addAttribute("action","Edit");
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("page", page);
         return "formPatient";
     }
+
+    @PostMapping("/admin/save")
+    public String save(Model model, @Valid Patient patient , BindingResult bindingResult,@RequestParam(name="keyword",defaultValue = "") String keyword,@RequestParam(name="page",defaultValue = "0") int page) {
+        if (bindingResult.hasErrors()) return "redirect:/admin/addPatient";
+        patientRepository.save(patient);
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
+    }
+
 }
